09ece8d755c832919136bafd316e8e34010092f9
Iarla <g00405899@atu.ie>
2024-02-14 16:11:44 +0000

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Page</title>
+  </head>
+  <body>
+    <h1>This is a HTML header!</h1>
+    <?php
+      echo "This is some php text!";
+    ?>
+    <a href="shop.html" class="button">Go to Shop</a>
+  </body>
+</html>
