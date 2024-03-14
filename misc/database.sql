3b1a4b93d779bb4429591fb5769997f246c1c278
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-03-14 03:22:56 +0000

    adjust values in sql file

--- misc/database.sql
+++ misc/database.sql
@@ -27,7 +27,7 @@ DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(250) NOT NULL DEFAULT '',
+  `username` varchar(250) NOT NULL DEFAULT '',
   `password` varchar(200) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
