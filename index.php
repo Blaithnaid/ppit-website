8e71175be251e5eab3617f7a6c9b50c727c4936d
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-03-14 01:55:02 +0000

    padding for flex content of website homepage

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>Nephin Media | Home</title>
 	<link rel="stylesheet" href="css/styles.css">
@@ -16,6 +17,7 @@
 			background-repeat: no-repeat;
 			background-size: cover;
 		}
+
 		footer {
 			margin-top: auto;
 			position: absolute;
@@ -25,8 +27,29 @@
 			background-color: #f2f2f2;
 			padding: 1px;
 		}
+		.flexcontent {
+			display: flex;
+			flex-direction: column;
+			flex-wrap: wrap;
+			justify-content: space-around;
+			align-items: center;
+			margin: 0;
+			padding-left: 20%;
+			padding-right: 20%;
+			width: 100%;
+			height: 100%;
+		}
+		.flexitem {
+			/* width: 100%; */
+			margin: 10px;
+			padding: 20px;
+			text-align: center;
+			background-color: #f2f2f2;
+			border: 1px solid black;
+		}
 	</style>
 </head>
+
 <body>
 	<div id="logogradient"></div>
 	<div class="logo">
@@ -45,18 +68,43 @@
 			<p>Welcome to Nephin Media</p>
 			<p>Here is some text about the site</p>
 		</main>
+		<?php
+		if (isset($_SESSION['x'])) {
+			echo "<div id='logoutbutton'><a href='logout.php'>Logout</a></div>";
+		} else {
+			echo "<div id='loginbutton'><a href='login.php'>Login</a></div>";
+		}
+		?>
 	</div>
-	<div >
+	<div class=flexcontent>
+		<div class="flexitem">
+			<h2>Heading 1</h2>
+			<p>Some text</p>
+		</div>
+		<div class="flexitem">
+			<h2>Heading 2</h2>
+			<p>Some text</p>
+		</div>
+		<div class="flexitem">
+			<h2>Heading 3</h2>
+			<p>Some text</p>
+		</div>
+		<div class="flexitem">
+			<h2>Heading 4</h2>
+			<p>Some text</p>
+		</div>
+		<div class="flexitem">
+			<h2>Heading 5</h2>
+			<p>Some text</p>
+		</div>
+		<div class="flexitem">
+			<h2>Heading 6</h2>
+			<p>Some text</p>
+		</div>
 	</div>
-<?php
-if (!isset($_SESSION['x'])) {
-	echo "<div id='logoutbutton'><a href='logout.php'>Logout</a></div>";
-} else {
-	echo "<div id='loginbutton'><a href='login.php'>Login</a></div>";
-}
-?>
 	<footer>
 		<p>&copy; 2020 Nephin Media</p>
 	</footer>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -1,84 +1,111 @@
 <?php
-session_start();
-if( isset($_SESSION['user_id']) ){
-	header('Location: home.php');
-}
 require_once 'functions.php';
+session_start();
+if (isset($_SESSION['user_id'])) {
+    header('Location: home.php');
+} else if (isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $connection = createConnection();
+    $query = "SELECT * FROM users WHERE username = '$username'";
+    $result = mysqli_query($connection, $query);
+    if (mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_assoc($result);
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['user_id'] = $row['id'];
+            $_SESSION['username'] = $row['username'];
+            header('Location: home.php');
+        } else {
+            echo "Invalid password";
+        }
+    } else {
+        echo "Invalid username";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Login</title>
-        <link rel="stylesheet" href="css/styles.css">
-        <style>
-            body {
-                margin: 0;
-                width: 100%;
-                background: url('img/background_scaled.png');
-                background-position: center;
-                background-repeat: no-repeat;
-                background-size: cover;
-            }
-            #loginbox {
-                position: absolute;
-                top: 50%;
-                left: 50%;
-                transform: translate(-50%, -50%);
-                width: 300px;
-                padding: 20px;
-                background: white;
-                border: 3px solid black;
-                text-align: center;
-            }
-            #loginbox input {
-                margin: 10px 0;
-                padding: 10px;
-                width: 100%;
-                box-sizing: border-box;
-            }
-            #loginbox input[type="submit"] {
-                background: #4CAF50;
-                color: white;
-                border: none; 
-            }
-            .logo {
-                position: absolute;
-                top: 0;
-                left: 0;
-                padding: 15px;
-            }
-            #back {
-                position: absolute;
-                top: 15px;
-                right: 10px;
-            }
-            #back a {
-                background-color: white;
-                text-decoration: none;
-                color: black;
-                border: 2px solid black;
-                padding: 5px;
-            }
-            #back a:hover {
-                background-color: black;
-                color: white;
-                border: 2px solid white;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="logo">
-            <img src="img/nephin_transp.png" height="120px" alt="">
-        </div>
-        <div id="back">
-            <a href="index.html">Back to Home</a>
-        </div>
-        <div id="loginbox">
-            <h1>Login</h1>
-            <form action="login.php" method="post">
-                <input type="text" name="username" placeholder="Username" required>
-                <input type="password" name="password" placeholder="Password" required>
-                <input type="submit" value="Login">
-            </form>
-        </div>
-    </body>
+
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <style>
+        body {
+            margin: 0;
+            width: 100%;
+            background: url('img/background_scaled.png');
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+        #loginbox {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 300px;
+            padding: 20px;
+            background: white;
+            border: 3px solid black;
+            text-align: center;
+        }
+
+        #loginbox input {
+            margin: 10px 0;
+            padding: 10px;
+            width: 100%;
+            box-sizing: border-box;
+        }
+
+        #loginbox input[type="submit"] {
+            background: #4CAF50;
+            color: white;
+            border: none;
+        }
+
+        .logo {
+            position: absolute;
+            top: 0;
+            left: 0;
+            padding: 15px;
+        }
+
+        #back {
+            position: absolute;
+            top: 15px;
+            right: 10px;
+        }
+
+        #back a {
+            background-color: white;
+            text-decoration: none;
+            color: black;
+            border: 2px solid black;
+            padding: 5px;
+        }
+
+        #back a:hover {
+            background-color: black;
+            color: white;
+            border: 2px solid white;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="logo">
+        <img src="img/nephin_transp.png" height="120px" alt="">
+    </div>
+    <div id="back">
+        <a href="index.html">Back to Home</a>
+    </div>
+    <div id="loginbox">
+        <h1>Login</h1>
+        <form action="login.php" method="post">
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <input type="submit" value="Login">
+        </form>
+    </div>
+</body>
