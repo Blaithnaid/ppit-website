9a9953a4d4c25e1bd7ac3f4c3164e95f6c59727e
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-03-16 01:51:39 +0000

    add placeholder image for homescreen

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 	<title>Nephin Media | Home</title>
 	<link rel="stylesheet" href="css/styles.css">
 	<style>
-		
 		footer {
 			margin-top: auto;
 			width: 100%;
@@ -13,24 +12,42 @@
 			background-color: #f2f2f2;
 			padding: 1px;
 		}
+
 		.flexcontent {
 			border-top: 30px solid gray;
 			display: flex;
-			flex-direction: column;
+			flex-direction: row;
 			flex-wrap: wrap;
 			justify-content: space-around;
 			align-items: center;
 			margin: 0;
-			padding-left: 20%;
-			padding-right: 20%;
+			padding-left: 10%;
+			padding-right: 10%;
 			height: 100%;
 		}
+
 		.flexitem {
+			min-height: 280px;
+			flex-wrap: wrap;
+			min-width: 40%;
 			margin: 10px;
 			padding: 20px;
 			text-align: center;
 			background-color: #f2f2f2;
 			border: 1px solid black;
+		}
+		.flexitem img {
+			width: 380px;
+			height: auto;
+		}
+
+		.spread_text {
+			position: absolute;
+			bottom: 10px;
+			right: auto;
+			margin: 0;
+			padding: 0;
+			color: white;
 		}
 	</style>
 </head>
@@ -40,7 +57,7 @@
 	<div class="logo">
 		<img src="img/nephin_transp.png" height="120px" alt="">
 	</div>
-	<div id="home_spread">
+	<div class="home_spread">
 		<header>
 			<nav>
 				<a href="index.php">Home</a>
@@ -49,12 +66,12 @@
 				<a href="gallery.php">Gallery</a>
 			</nav>
 		</header>
-		<main>
+		<div id="spread_text">
 			<p>Welcome to Nephin Media</p>
 			<p>Here is some text about the site</p>
-		</main>
+		</div>
 		<?php
-		if (isset($_SESSION['x'])) {
+		if (isset ($_SESSION['x'])) {
 			echo "<div class='logoutbutton'><a href='logout.php'>Logout</a></div>";
 		} else {
 			echo "<div class='loginbutton'><a href='login.php'>Login</a></div>";
@@ -63,26 +80,32 @@
 	</div>
 	<div class=flexcontent>
 		<div class="flexitem">
+			<img src="img/lockscreen.png" alt="">
 			<h2>Heading 1</h2>
 			<p>Some text</p>
 		</div>
 		<div class="flexitem">
+		<img src="img/lockscreen.png" alt="">
 			<h2>Heading 2</h2>
 			<p>Some text</p>
 		</div>
 		<div class="flexitem">
+		<img src="img/lockscreen.png" alt="">
 			<h2>Heading 3</h2>
 			<p>Some text</p>
 		</div>
 		<div class="flexitem">
+		<img src="img/lockscreen.png" alt="">
 			<h2>Heading 4</h2>
 			<p>Some text</p>
 		</div>
 		<div class="flexitem">
+		<img src="img/lockscreen.png" alt="">
 			<h2>Heading 5</h2>
 			<p>Some text</p>
 		</div>
 		<div class="flexitem">
+		<img src="img/lockscreen.png" alt="">
 			<h2>Heading 6</h2>
 			<p>Some text</p>
 		</div>
