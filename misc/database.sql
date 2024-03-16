827aa6351e088ac85d961149cf5e78d8a882b0e6
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-03-16 18:12:49 +0000

    update users table

--- misc/database.sql
+++ misc/database.sql
@@ -30,16 +30,17 @@ CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(250) NOT NULL DEFAULT '',
   `password` varchar(200) NOT NULL DEFAULT '',
+  `isAdmin` BOOLEAN NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 
-INSERT INTO `users` (`id`, `email`, `password`)
+INSERT INTO `users` (`id`, `username`, `password`, `isAdmin` x)
 VALUES
-	(1,'admin','$2y$10$n8NIJ5zsmtIxASEZSzWyi.7Pu50x5Z8F1uGcTiNA4rEXbS78EOuha'),
-	(2,'email123@email.com','$2y$10$9OOFy5W.Vz0VapyS5U.ZqujRiXnklh48Nhc3qGfdSKxLbXRH3i87m');
+	(1,'admin','$2y$10$n8NIJ5zsmtIxASEZSzWyi.7Pu50x5Z8F1uGcTiNA4rEXbS78EOuha',true),
+	(2,'email123@email.com','$2y$10$9OOFy5W.Vz0VapyS5U.ZqujRiXnklh48Nhc3qGfdSKxLbXRH3i87m',false);
 
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
