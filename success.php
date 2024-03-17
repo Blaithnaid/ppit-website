a4f83df8ee7332b257b6961f8b8599fe07a06fe7
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-03-17 15:03:58 +0000

    updating login success page

--- success.php
+++ success.php
@@ -1,5 +1,31 @@
+<?php
+require_once 'functions.php'; // include the functions file so we can use createConnection() and validateUser()
+session_start(); // start or resume the session
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+<!DOCTYPE html>
 <html>
-    <h1>
-        Account created successfully
-    </h1>
+
+<head>
+	<title>Nephin Media | Success!</title>
+	<link rel="stylesheet" href="css/styles.css">
+	<style>
+		body {
+			margin: 0;
+			width: 100%;
+			background: url('img/background_scaled.png');
+			background-position: top center;
+			background-repeat: no-repeat;
+			background-size: cover;
+		}
+	</style>
+	<link rel="stylesheet" href="css/forms.css">
+</head>
+
+<body>
+	<div class="logo">
+		<img src="img/nephin_transp.png" height="120px" alt="">
+	</div>
+</body>
 </html>
