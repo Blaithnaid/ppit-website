55dfc991a51ce166580fbe6edaa5778617a858b8
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-03-19 23:06:21 +0000

    cleaning up validation logic

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ function createConnection()
     // Create connection
     $connection = new mysqli($servername, $username, $password, $dbname);
     if (!$connection) {
-        die ("Connection failed: " . mysqli_connect_error());
+        die("Connection failed: " . mysqli_connect_error());
     }
     return $connection;
 }
@@ -17,7 +17,7 @@ function createConnection()
 function redirectLogin()
 {
     // set a variable in the session to indicate that the user tried to access a page without logging in
-    $_SESSION['triedNoLogin'] = "no";
+    $_SESSION["triedNoLogin"] = "no";
     header("Location: login.php");
     exit();
 }
@@ -29,13 +29,12 @@ function validateUser($username, $password)
     $result = mysqli_query($conn, $query);
     if (mysqli_num_rows($result) == 1) {
         $row = mysqli_fetch_assoc($result);
-        if (password_verify($password, $row['password'])) {
-            $_SESSION['user_id'] = $row['id'];
-            $_SESSION['username'] = $row['username'];
+        if (password_verify($password, $row["password"])) {
+            $_SESSION["user_id"] = $row["id"];
+            $_SESSION["username"] = $row["username"];
             return true;
         }
         return "A user with that username does not exist. Please try again.";
-        // returning two separate outcomes isn't very secure, but I just think it looks fancier
     }
 }
 function usernameExists($username)
