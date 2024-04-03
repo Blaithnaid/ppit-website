c2cf6a1cb952fa60251d1c48c4e7bc1465d795ec
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-04-03 04:53:54 +0100

    reformatting, adding 'submissions' table

--- misc/nephin_db.sql
+++ misc/nephin_db.sql
@@ -1,17 +1,15 @@
 DROP DATABASE IF EXISTS `nephin_admin`;
 CREATE DATABASE `nephin_admin`;
 USE `nephin_admin`;
-
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(250),
-  `password` varchar(255) NOT NULL, 
+  `password` varchar(255) NOT NULL,
   `isAdmin` BOOLEAN NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
 INSERT INTO `users` (`id`, `username`, `password`, `isAdmin`)
 VALUES (
     1,
@@ -25,14 +23,17 @@ VALUES (
     '$2y$10$9OOFy5W.Vz0VapyS5U.ZqujRiXnklh48Nhc3qGfdSKxLbXRH3i87m',
     false
   );
-
 DROP TABLE IF EXISTS `submissions`;
 CREATE TABLE `submissions` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `submission_type` SET ('booking', 'contact', 'support') NOT NULL, 
-  `user_id` int(10) unsigned NOT NULL, 
-  `when_submitted` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
+  `submission_type`
+  SET ('booking', 'contact', 'support') NOT NULL,
+    `email` varchar(250) NOT NULL,
+    `name` varchar(250) NOT NULL,
+    `message` TEXT NOT NULL,
+    `status` ENUM('pending', 'resolved') NOT NULL DEFAULT 'pending',
+    `when_submitted` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
+INSERT INTO `submissions` (`submission_type`, `email`, `name`, `message`)
+VALUES ('booking', 'test@test.com', 'test', 'test message');
